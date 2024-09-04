7eaee62850ae7e277093f7b8c25547bb89645f02
ydmins <dr.ydmins@gmail.com>
2024-09-04 22:42:24 +0900

    chore : essential spring project files added

---
+++ src/main/java/com/ydmins/concurrency_control/ConcurrencyControlApplication.java
@@ -0,0 +1,13 @@
+package com.ydmins.concurrency_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ydmins/concurrency_control/ConcurrencyControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydmins.concurrency_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
